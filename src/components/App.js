c23a1a1d86868ef506399747b62cf3edcd91895f
Scott Woodend <woodendscott@gmail.com>
2017-12-05 17:33:40 -0500

    renders the tablearea

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from './Navbar';
+import TableArea from './TableArea';
 import '../styles/app.css';
 
 const App = () => {
     return (
         <div className="app">
             <Navbar/>
+            <TableArea/>
         </div>
     
     )
