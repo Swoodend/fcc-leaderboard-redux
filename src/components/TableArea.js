e389d47a737e74c3fb1dbcaae27f469560ba2a3c
Scott Woodend <woodendscott@gmail.com>
2017-12-05 17:28:06 -0500

    render table area component

---
+++ src/components/TableArea.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Table from './Table';
+import "../styles/tablearea.css";
+
+const TableArea = () => {
+    return (
+        <div className="table-area-container">
+            <Table/>
+        </div>
+    )
+}
+
+
+
+export default TableArea
