b049cbbb8644869dbe0b94cb90ef2af157b7cd3b
Scott Woodend <woodendscott@gmail.com>
2017-12-07 14:25:11 -0500

    add actions to fetch all data, sort by recent, sorty by all time

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,26 +1,73 @@
 //this will flip fetchingUsers state to true and can then display a loading message
-export function fetchUsersRecent(){
+export function fetchingData(bool){
     return {
-        type: "FETCH_USERS_RECENT"
+        type: "FETCHING_DATA",
+        payload: bool
     }
 }
-//this will flip fetchingUsers state to true and can then display a loading message
-export function fetchUsersAllTime(){
-    return {
-        type:"FETCH_USERS_ALL_TIME"
-    }
-}
+
 //this willl flip fetchingAllTime or fetchingRecent to false and can display data table
-export function fetchComplete(fetchType, data){
+function updateUserData(fetchType, data){
     return {
-        type: "FETCH_COMPLETE",
+        type: "SET_DATA",
         fetchType,
         payload: data
     }
 }
 //this will catch errors associated with fetching and display
-export function reportFetchError(){
+
+function fetchRecentData(){
+    return fetch('https://fcctop100.herokuapp.com/api/fccusers/top/recent')
+    .then((res) => {
+        return res.json();
+    })
+    .then((recentData) => {
+        return recentData
+    })
+}
+
+function fetchAllTimeData(){
+    return fetch('https://fcctop100.herokuapp.com/api/fccusers/top/alltime')
+    .then((res) => {
+        return res.json();
+    })
+    .then((allTimeData) => {
+        return allTimeData;
+    })
+}
+
+export function reportFetchError(e){
     return {
-        type: "FETCH_ERROR"
+        type: "FETCH_ERROR",
+        payload: e
+    }
+}
+
+export function sortByRecent(){
+    return {
+        type: "SORT_RECENT"
+    }
+}
+
+export function sortByAllTime(){
+    return {
+        type: "SORT_ALL_TIME"
+    }
+}
+
+export function getAllData(){
+
+    return (dispatch) => {
+        Promise.all([fetchRecentData(), fetchAllTimeData()])
+        .then(([recent, allTime]) => {
+            dispatch(fetchingData(false));            
+            dispatch(updateUserData('recent', recent));
+            dispatch(updateUserData('allTime', allTime));
+            
+        })
+        .catch((e) => {
+            console.log('error', e);
+            dispatch(reportFetchError(e));
+        });
     }
 }
