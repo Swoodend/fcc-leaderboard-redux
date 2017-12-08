8385e2112408cc8538b72342bfa6f3bbb97828b0
Scott Woodend <woodendscott@gmail.com>
2017-12-08 16:16:19 -0500

    remove unnecessary imports

--- tests/actions.test.js
+++ tests/actions.test.js
@@ -1,5 +1,3 @@
-import React from 'react';
-import { shallow } from 'enzyme';
 import * as actions from '../src/actions/actions';
 import configureMockStore from 'redux-mock-store'
 import thunk from 'redux-thunk';
